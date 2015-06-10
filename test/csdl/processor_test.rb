b8c19a72991474e8c8c44aafb6f48170851f83e5
BJ Neilsen <bj.neilsen@gmail.com>
2015-06-10 16:18:19 -0600

    Test grouped expression processing

--- test/csdl/processor_test.rb
+++ test/csdl/processor_test.rb
@@ -100,81 +100,55 @@ class ProcessorTest < ::MiniTest::Unit::TestCase
     assert_csdl_matches(expected, sexp)
   end
 
+  def test_grouped_expressions
+    expected = %q{(fb.content contains_any "foo" OR fb.parent.content contains_any "foo") AND (fb.content contains_any "bar" OR fb.parent.content contains_any "bar")}
+    sexp = \
+      s(:and,
+        s(:closure,
+          s(:or,
+            s(:where,
+              s(:target, "fb.content"),
+              s(:operator, :contains_any),
+              s(:argument,
+                s(:string, "foo")
+               )
+             ),
+             s(:where,
+               s(:target, "fb.parent.content"),
+               s(:operator, :contains_any),
+               s(:argument,
+                 s(:string, "foo")
+                )
+              ),
+           ),
+         ),
+         s(:closure,
+           s(:or,
+             s(:where,
+               s(:target, "fb.content"),
+               s(:operator, :contains_any),
+               s(:argument,
+                 s(:string, "bar")
+                )
+              ),
+              s(:where,
+                s(:target, "fb.parent.content"),
+                s(:operator, :contains_any),
+                s(:argument,
+                  s(:string, "bar")
+                 )
+               ),
+            )
+          )
+       )
+
+    assert_csdl_matches(expected, sexp)
+  end
+
   private
 
   def assert_csdl_matches(expected, sexp)
     assert_equal(expected, ::CSDL::Processor.new.process(sexp))
   end
-
-# csdl_sexp = s(:closure,
-#               s(:or,
-#                 s(:where,
-#                   s(:target, "fb.content"),
-#                   s(:operator, :contains_any),
-#                   s(:argument,
-#                     s(:string, "foo")
-#                    )
-#                  ),
-#                  s(:where,
-#                    s(:target, "fb.parent.content"),
-#                    s(:operator, :contains_any),
-#                    s(:argument,
-#                      s(:string, "foo")
-#                     )
-#                   )
-#                )
-#              )
-#
-# puts
-# puts csdl_sexp.to_sexp
-#
-# puts
-# puts "processing..."
-# processed = CSDL.new.process(csdl_sexp)
-# puts processed
-#
-# builder = CSDLBuilder.new._or do
-#   [
-#     CSDLBuilder.new.closure {
-#       CSDLBuilder.new._and {
-#         [
-#           CSDLBuilder.new.closure {
-#             CSDLBuilder.new._or {
-#               [
-#                 CSDLBuilder.new.where("fb.content", :contains_any, "ebola"),
-#                 CSDLBuilder.new.where("fb.parent.content", :contains_any, "ebola")
-#               ]
-#             }
-#           },
-#           CSDLBuilder.new._not("fb.content", :contains_any, "government,politics"),
-#           CSDLBuilder.new.where("fb.author.country_code", :in, "GB")
-#         ]
-#       }
-#     },
-#     CSDLBuilder.new.closure {
-#       CSDLBuilder.new._and {
-#         [
-#           CSDLBuilder.new.closure {
-#             CSDLBuilder.new._or {
-#               [
-#                 CSDLBuilder.new.where("fb.content", :contains_any, "malta,malta island,#malta"),
-#                 CSDLBuilder.new.where("fb.parent.content", :contains_any, "malta,malta island,#malta")
-#               ]
-#             }
-#           },
-#           CSDLBuilder.new._not("fb.content", :contains_any, "vacation,suicide,poker awards")
-#         ]
-#       }
-#     }
-#   ]
-# end
-#
-# puts
-# puts "Builder sexp..."
-# puts builder.to_sexp
-#
-# puts
-# puts "processing built sexp..."
-# puts CSDL.new.process(builder)
 
 end
