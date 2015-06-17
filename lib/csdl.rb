e160606f12ebdd7f567901a4225b22eb91899f17
BJ Neilsen <bj.neilsen@gmail.com>
2015-06-17 12:01:19 -0600

    Add high-level documentation to CSDL module

--- lib/csdl.rb
+++ lib/csdl.rb
@@ -1,6 +1,15 @@
 require "ast"
 require "csdl/version"
 
+# CSDL is a library for manipulating Abstract Syntax Trees that represent raw CSDL defintions. Using an AST
+# makes it much simpler to manipulate, traverse, validate, and test complex CSDL queries.
+#
+# Use the DSL {Builder} class to produce a tree of nodes, then process those nodes
+# with {Processor}, {InteractionFilterProcessor}, or {QueryFilterProcessor}. See
+# those individuals classes for usage documentation.
+#
+# @see http://dev.datasift.com/docs/csdl DataSift CSDL Language Documentation
+#
 module CSDL
 end
 
