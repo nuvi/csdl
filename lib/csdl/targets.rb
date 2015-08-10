a1e4bac5a9c69fadfed0cb6762c15715008024ca
BJ Neilsen <bj.neilsen@gmail.com>
2015-08-10 10:36:53 -0600

    Add regions for additional 37 new countries

--- lib/csdl/targets.rb
+++ lib/csdl/targets.rb
@@ -120,27 +120,62 @@ module CSDL
         "Steiermark" , "Tirol"   , "Vorarlberg"       , "Wien"
       ].freeze,
 
-      "Bahrain" => [],
+      "Bahrain" => [
+        "Al Muharraq"
+      ].freeze,
 
       "Belgium" => [
         "Région De Bruxelles-Capitale"
       ].freeze,
 
-      "Bulgaria" => [],
+      "Bulgaria" => [
+        "Blagoevgrad" , "Burgas"       , "Dobrich"     , "Gabrovo" , "Grad Sofiya"    ,
+        "Khaskovo"    , "Kurdzhali"    , "Kyustendil"  , "Lovech"  , "Montana"        ,
+        "Pazardzhik"  , "Pernik"       , "Pleven"      , "Plovdiv" , "Razgrad"        ,
+        "Ruse"        , "Shumen"       , "Silistra"    , "Sliven"  , "Smolyan"        ,
+        "Sofiya"      , "Stara Zagora" , "Turgovishte" , "Varna"   , "Veliko Turnovo" ,
+        "Vidin"       , "Vratsa"       , "Yambol"
+      ].freeze,
 
-      "Croatia" => [],
+      "Croatia" => [
+        "Bjelovarsko-Bilogorska" , "Brodsko-Posavska"   , "Dubrovacko-Neretvanska" ,
+        "Grad Zagreb"            , "Istarska"           , "Karlovacka"             ,
+        "Koprivnicko-Krizevacka" , "Krapinsko-Zagorska" , "Medimurska"             ,
+        "Osjecko-Baranjska"      , "Primorsko-Goranska" , "Sisacko-Moslavacka"     ,
+        "Splitsko-Dalmatinska"   , "Varazdinska"        , "Viroviticko-Podravska"  ,
+        "Vukovarsko-Srijemska"   , "Zadarska"           , "Zagrebacka"
+      ].freeze,
 
-      "Cyprus" => [],
+      "Cyprus" => [
+        "Famagusta", "Kyrenia", "Larnaca", "Limassol", "Paphos"
+      ].freeze,
 
-      "Czech Republic" => [],
+      "Czech Republic" => [
+        "Hlavni Mesto Praha" , "Hradec Kralove" , "Jihocesky Kraj"       , "Jihomoravsky Kraj" ,
+        "Karlovarsky Kraj"   , "Liberecky Kraj" , "Moravskoslezsky Kraj" , "Olomoucky Kraj"    ,
+        "Pardubicky Kraj"    , "Plzensky Kraj"  , "Stredocesky Kraj"     , "Ustecky Kraj"      ,
+        "Vysocina"           , "Zlinsky Kraj"
+      ].freeze,
 
       "Denmark" => [
         "Arhus", "Nordjylland"
       ].freeze,
 
-      "Egypt" => [],
+      "Egypt" => [
+        "Ad Daqahliyah"  , "Al Bahr al Ahmar" , "Al Buhayrah"     ,
+        "Al Fayyum"      , "Al Gharbiyah"     , "Al Iskandariyah" ,
+        "Al Isma'iliyah" , "Al Jizah"         , "Al Minufiyah"    ,
+        "Al Minya"       , "Al Qahirah"       , "Al Qalyubiyah"   ,
+        "As Suways"      , "Ash Sharqiyah"    , "Aswan"           ,
+        "Asyut"          , "Bani Suwayf"      , "Bur Sa'id"       ,
+        "Dumyat"         , "Janub Sina'"      , "Kafr ash Shaykh" ,
+        "Matruh"         , "Qina"             , "Shamal Sina'"    ,
+        "Suhaj"
+      ].freeze,
 
-      "Estonia" => [],
+      "Estonia" => [
+        "Harjumaa", "Tartumaa"
+      ].freeze,
 
       "Finland" => [
         "Åland", "Lapland"
@@ -162,15 +197,30 @@ module CSDL
         "Sachsen"           , "Sachsen-Anhalt"      , "Schleswig-Holstein" , "Thüringen"
       ].freeze,
 
-      "Ghana" => [],
+      "Ghana" => [
+        "Ashanti", "Greater Accra"
+      ].freeze,
 
-      "Greece" => [],
+      "Greece" => [
+        "Attiki", "Thessaloniki"
+      ].freeze,
 
-      "Hungary" => [],
+      "Hungary" => [
+        "Bacs-Kiskun"            , "Baranya"           , "Bekes"             , "Borsod-Abauj-Zemplen" , "Budapest" ,
+        "Csongrad"               , "Fejer"             , "Gyor-Moson-Sopron" , "Hajdu-Bihar"          , "Heves"    ,
+        "Jasz-Nagykun-Szolnok"   , "Komarom-Esztergom" , "Nograd"            , "Pest"                 , "Somogy"   ,
+        "Szabolcs-Szatmar-Bereg" , "Tolna"             , "Vas"               , "Veszprem"             , "Zala"
+      ].freeze,
 
       "Iceland" => [].freeze,
 
-      "Iraq" => [],
+      "Iraq" => [
+        "Al Anbar"     , "Al Basrah" , "Al Muthanna"     , "Al Qadisiyah" ,
+        "An Najaf"     , "Arbil"     , "As Sulaymaniyah" , "At Ta'mim"    ,
+        "Babil"        , "Baghdad"   , "Dahuk"           , "Dhi Qar"      ,
+        "Diyala"       , "Karbala'"  , "Maysan"          , "Ninawa"       ,
+        "Salah ad Din" , "Wasit"
+      ].freeze,
 
       "Ireland" => [
         "Carlow"  , "Cavan"     , "Clare"     , "Cork"      , "Donegal"   ,
@@ -181,7 +231,9 @@ module CSDL
         "Wicklow"
       ].freeze,
 
-      "Israel" => [],
+      "Israel" => [
+        "HaDarom", "HaMerkaz", "HaTzafon", "Heifa", "Tel Aviv", "Yerushalayim"
+      ].freeze,
 
       "Italy" => [
         "Abruzzo"               , "Basilicata"          , "Calabria" , "Campania"      , "Emilia-Romagna" ,
@@ -190,29 +242,47 @@ module CSDL
         "Toscana"               , "Trentino-Alto Adige" , "Umbria"   , "Valle d'Aosta" , "Veneto"
       ].freeze,
 
-      "Jordan" => [],
+      "Jordan" => [
+        "Al Balqa'" , "Al Karak" , "Al Mafraq" , "Amman" , "At Tafilah" ,
+        "Az Zarqa"  , "Irbid"    , "Ma"
+      ].freeze,
 
-      "Kenya" => [],
+      "Kenya" => [
+        "Central", "Coast", "Nairobi", "Rift Valley"
+      ].freeze,
 
-      "Kuwait" => [],
+      "Kuwait" => [].freeze,
 
-      "Latvia" => [],
+      "Latvia" => [].freeze,
 
-      "Lebanon" => [],
+      "Lebanon" => [
+        "Beyrouth", "Liban-Nord", "Liban-Sud", "Mont-Liban"
+      ].freeze,
 
-      "Lithuania" => [],
+      "Lithuania" => [
+        "Kauno Apskritis", "Klaipedos Apskritis", "Panevezio Apskritis", "Siauliu Apskritis", "Vilniaus Apskritis"
+      ].freeze,
 
       "Luxembourg" => [
         "Diekirch", "Grevenmacher", "Luxembourg"
       ].freeze,
 
-      "Macedonia, the former Yugoslav Republic of" => [],
+      "Macedonia, the former Yugoslav Republic of" => [
+        "Kumanovo" , "Ohrid" , "Prilep" , "Struga"   , "Strumica" ,
+        "Tetovo"   , "Veles" , "Stip"   , "Gostivar"
+      ].freeze,
 
-      "Malta" => [],
+      "Malta" => [
+      ].freeze,
 
-      "Mauritius" => [],
+      "Mauritius" => [
+        "Flacq"      , "Grand Port"         , "Pamplemousses" , "Plaines Wilhems" ,
+        "Port Louis" , "Riviere du Rempart"
+      ].freeze,
 
-      "Morocco" => [],
+      "Morocco" => [
+        "Casablanca", "Fes", "Marrakech", "Meknes", "Rabat-Sale", "Tanger", "Taza"
+      ].freeze,
 
       "Netherlands" => [
         "Drenthe" , "Flevoland"     , "Friesland"     , "Gelderland" , "Groningen" ,
@@ -220,7 +290,13 @@ module CSDL
         "Zeeland" , "Zuid-Holland"
       ].freeze,
 
-      "Nigeria" => [],
+      "Nigeria" => [
+        "Abia"   , "Akwa Ibom" , "Anambra" , "Benue"                     , "Cross River" ,
+        "Delta"  , "Edo"       , "Enugu"   , "Federal Capital Territory" , "Imo"         ,
+        "Kaduna" , "Kano"      , "Kwara"   , "Lagos"                     , "Ogun"        ,
+        "Ondo"   , "Osun"      , "Oyo"     , "Plateau"                   , "Rivers"      ,
+        "Niger"  , "Adamawa"   , "Ekiti"
+      ].freeze,
 
       "Norway" => [
         "Akershus" , "Aust-Agder"       , "Buskerud"        , "Finnmark"       ,
@@ -230,11 +306,18 @@ module CSDL
         "Troms"    , "Vest-Agder"       , "Vestfold"
       ].freeze,
 
-      "Oman" => [],
+      "Oman" => [
+        "Al Batinah", "Masqat", "Zufar"
+      ].freeze,
 
-      "Palestinian territories" => [],
+      "Palestinian territories" => [].freeze,
 
-      "Poland" => [],
+      "Poland" => [
+        "Dolnoslaskie"        , "Kujawsko-Pomorskie" , "Lodzkie"            , "Lubelskie"      ,
+        "Lubuskie"            , "Malopolskie"        , "Opolskie"           , "Podkarpackie"   ,
+        "Podlaskie"           , "Pomorskie"          , "Slaskie"            , "Swietokrzyskie" ,
+        "Warminsko-Mazurskie" , "Wielkopolskie"      , "Zachodniopomorskie"
+      ].freeze,
 
       "Portugal" => [
         "Aveiro"         , "Azores"  , "Beja"             , "Braga"      , "Braganca" ,
@@ -243,19 +326,41 @@ module CSDL
         "Santarem"       , "Setubal" , "Viana do Castelo" , "Vila Real"  , "Viseu"
       ].freeze,
 
-      "Qatar" => [],
+      "Qatar" => [
+        "Ad Dawhah"
+      ].freeze,
 
-      "Romania" => [],
+      "Romania" => [
+        "Alba"            , "Arad"      , "Arges"         , "Bacau"     , "Bihor"     ,
+        "Bistrita-Nasaud" , "Botosani"  , "Braila"        , "Brasov"    , "Bucuresti" ,
+        "Buzau"           , "Calarasi"  , "Caras-Severin" , "Cluj"      , "Constanta" ,
+        "Covasna"         , "Dambovita" , "Dolj"          , "Galati"    , "Giurgiu"   ,
+        "Gorj"            , "Harghita"  , "Hunedoara"     , "Ialomita"  , "Iasi"      ,
+        "Ilfov"           , "Maramures" , "Mehedinti"     , "Mures"     , "Neamt"     ,
+        "Olt"             , "Prahova"   , "Salaj"         , "Satu Mare" , "Sibiu"     ,
+        "Suceava"         , "Teleorman" , "Timis"         , "Tulcea"    , "Valcea"    ,
+        "Vaslui"          , "Vrancea"
+      ].freeze,
 
-      "Saudi Arabia" => [],
+      "Saudi Arabia" => [
+        "Al Jawf" , "Al Madinah" , "Al Qasim" , "Ar Riyad" , "Ash Sharqiyah" ,
+        "Ha'il"   , "Jizan"      , "Makkah"   , "Tabuk"
+      ].freeze,
 
-      "Serbia" => [],
+      "Serbia" => [].freeze,
 
-      "Slovakia" => [],
+      "Slovakia" => [
+        "Banska Bystrica" , "Bratislava" , "Kosice" , "Nitra" , "Presov" ,
+        "Trencin"         , "Trnava"     , "Zilina"
+      ].freeze,
 
-      "Slovenia" => [],
+      "Slovenia" => [].freeze,
 
-      "South Africa" => [],
+      "South Africa" => [
+        "Eastern Cape" , "Free State" , "Gauteng"    , "KwaZulu-Natal" ,
+        "Limpopo"      , "Mpumalanga" , "North-West" , "Northern Cape" ,
+        "Western Cape"
+      ].freeze,
 
       "Spain" => [
         "Andalucía"            , "Aragón"             , "Asturias" , "Cantabria"           ,
@@ -282,13 +387,43 @@ module CSDL
         "Zürich"
       ].freeze,
 
-      "Tunisia" => [],
+      "Tunisia" => [
+        "Al Mahdiyah" , "Al Munastir"  , "Al Qasrayn" , "Al Qayrawan" ,
+        "Bajah"       , "Banzart"      , "Bin"        , "Jundubah"    ,
+        "Kef"         , "Madanin"      , "Nabul"      , "Qabis"       ,
+        "Safaqis"     , "Sidi Bu Zayd" , "Susah"      , "Tunis"
+      ].freeze,
 
-      "Turkey" => [],
+      "Turkey" => [
+        "Adana"     , "Adiyaman"      , "Afyonkarahisar" , "Ağrı"      , "Amasya"    ,
+        "Ankara"    , "Antalya"       , "Artvin"         , "Aydin"     , "Balikesir" ,
+        "Batman"    , "Bilecik"       , "Bingöl"         , "Bitlis"    , "Bolu"      ,
+        "Burdur"    , "Bursa"         , "Çanakkale"      , "Cankiri"   , "Çorum"     ,
+        "Denizli"   , "Diyarbakir"    , "Edirne"         , "Elazığ"    , "Erzincan"  ,
+        "Erzurum"   , "Eskişehir"     , "Gaziantep"      , "Giresun"   , "Gumushane" ,
+        "Hakkari"   , "Hatay"         , "Icel"           , "Isparta"   , "Istanbul"  ,
+        "Izmir"     , "Kahramanmaraş" , "Karaman"        , "Kars"      , "Kastamonu" ,
+        "Kayseri"   , "Kilis"         , "Kirklareli"     , "Kırşehir"  , "Kocaeli"   ,
+        "Konya"     , "Kütahya"       , "Malatya"        , "Manisa"    , "Muğla"     ,
+        "Muş"       , "Nevşehir"      , "Nigde"          , "Ordu"      , "Osmaniye"  ,
+        "Rize"      , "Sakarya"       , "Samsun"         , "Şanlıurfa" , "Sinop"     ,
+        "Sirnak"    , "Sivas"         , "Tekirdağ"       , "Tokat"     , "Trabzon"   ,
+        "Tunceli"   , "Uşak"          , "Van"            , "Yalova"    , "Yozgat"    ,
+        "Zonguldak" , "Ardahan"
+      ].freeze,
 
-      "Ukraine" => [],
+      "Ukraine" => [
+        "Dnipropetrovs'ka Oblast'" , "Donets'ka Oblast'"     , "Ivano-Frankivs'ka Oblast'" ,
+        "Kharkivs'ka Oblast'"      , "Krym"                  , "Kyyivs'ka Oblast'"         ,
+        "L'vivs'ka Oblast'"        , "Odes'ka Oblast'"       , "Vinnyts'ka Oblast'"        ,
+        "Zakarpats'ka Oblast'"     , "Zaporiz'ka Oblast'"    , "Cherkas'ka Oblast'"        ,
+        "Chernivets'ka Oblast'"    , "Mikolayivs'ka Oblast'" , "Poltavs'ka Oblast'"        ,
+        "Rivnens'ka Oblast'"       , "Ternopil's'ka Oblast'" , "Volyns'ka Oblast'"
+      ].freeze,
 
-      "United Arab Emirates" => [],
+      "United Arab Emirates" => [
+        "Abu Dhabi", "Ajman", "Dubai", "Fujairah", "Ras Al Khaimah", "Sharjah"
+      ].freeze,
 
       "United Kingdom" => [
         "England", "Scotland", "Wales", "Northern Ireland"
